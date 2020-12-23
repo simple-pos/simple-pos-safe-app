fe4339b15f492e31510c648ece5d2140d9496315
Mikhail <mmvsha73@gmail.com>
2020-12-23 18:58:24 +0400

    update sdk version, prepare deployment

--- src/api/pos.ts
+++ src/api/pos.ts
@@ -1,25 +1,31 @@
 import * as ethers from "ethers"
 import SimplePOS from "../contracts/SimplePOS.json"
 
-const createPOS = (
+const getPOSDeploymentData = (
   exchangeAddress: string,
   curveCoefficient: number,
   commission: number,
   initialRatio: number,
   sposTokenName: string,
   sposTokenSymbol: string,
-) => {
+): string => {
   const contract = new ethers.ContractFactory(SimplePOS.abi, SimplePOS.bytecode)
-  const deploymentData = contract.interface.encodeFunctionData("constructor", [
+  const { data: deploymentData } = contract.getDeployTransaction(
     exchangeAddress,
     sposTokenName,
     sposTokenSymbol,
     initialRatio,
-    commission,
-    curveCoefficient,
-  ])
+    commission * 10000,
+    curveCoefficient * 10000,
+  )
 
-  console.log(deploymentData)
+  if (typeof deploymentData !== "string") {
+    throw new Error(
+      `Error encoding deployment data, received value isn't string. Value: ${deploymentData}`,
+    )
+  }
+
+  return deploymentData
 }
 
-export { createPOS }
+export { getPOSDeploymentData }
