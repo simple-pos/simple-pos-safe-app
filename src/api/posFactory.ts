b0730e119d39c9a5eac8dda4b26c87e4d88532d5
Mikhail Mikheev <mmvsha73@gmail.com>
2021-01-06 23:17:08 +0400

    update factory address

--- src/api/posFactory.ts
+++ src/api/posFactory.ts
@@ -2,7 +2,7 @@ import * as ethers from "ethers"
 import SimplePOSFactory from "../contracts/SimplePOSFactory.json"
 import { provider } from "./provider"
 
-const SPOS_FACTORY_ADDRESS_RINKEBY = "0x11B602B81935272ccE70031631C151EeA165e36a"
+const SPOS_FACTORY_ADDRESS_RINKEBY = "0x2721FF699963CE97297F22b82fde933Eb33cF1B3"
 
 const getPOSDeploymentData = (
   exchangeAddress: string,
